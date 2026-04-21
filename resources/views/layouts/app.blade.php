4ee91c33e0743e1dd65f698f0e52f5843b05b445
tahtaanugrah <tahtaanugrah@gmail.com>
2026-04-21 09:34:32 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/libs/bootstrap/dist/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/style.min.css') }}">
+</head>
+
+<body>
+
+<div id="main-wrapper"
+     data-layout="vertical"
+     data-navbarbg="skin6"
+     data-sidebartype="full"
+     data-sidebar-position="fixed"
+     data-header-position="fixed"
+     data-boxed-layout="full">
+
+    <!-- SIDEBAR -->
+    @include('layouts.sidebar')
+
+    <!-- NAVBAR -->
+    @include('layouts.navbar')
+
+    <!-- CONTENT -->
+    <div class="page-wrapper">
+        <div class="container-fluid">
+            @yield('content')
+        </div>
+    </div>
+
+</div>
+
+<!-- JS -->
+<script src="{{ asset('assets/libs/jquery/dist/jquery.min.js') }}"></script>
+<script src="{{ asset('assets/libs/popper.js/dist/umd/popper.min.js') }}"></script>
+<script src="{{ asset('assets/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('assets/js/app-style-switcher.js') }}"></script>
+<script src="{{ asset('assets/js/feather.min.js') }}"></script>
+<script src="{{ asset('assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js') }}"></script>
+<script src="{{ asset('assets/js/sidebarmenu.js') }}"></script>
+<script src="{{ asset('assets/js/custom.min.js') }}"></script>
+
+<script>
+    feather.replace();
+</script>
+
+</body>
+</html>
